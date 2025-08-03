a1e4ba4ce692262418fc2d452b187239f9c1d791
JagadeeshKondepu <221210059@nitdelhi.ac.in>
2025-08-03 18:54:36 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import express  from "express"
 import cors from 'cors'
 import { connectDB } from "./config/db.js"
@@ -30,4 +31,38 @@ app.get("/", (req, res) => {
     res.send("API Working")
   });
 
+=======
+import express  from "express"
+import cors from 'cors'
+import { connectDB } from "./config/db.js"
+import userRouter from "./routes/userRoute.js"
+import foodRouter from "./routes/foodRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000;
+
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// db connection
+connectDB()
+
+// api endpoints
+app.use("/api/user", userRouter)
+app.use("/api/food", foodRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/cart", cartRouter)
+app.use("/api/order",orderRouter)
+
+app.get("/", (req, res) => {
+    res.send("API Working")
+  });
+
+>>>>>>> 9bdc29b (Initial commit: BiteBytes food delivery application with frontend, backend, and admin panel)
 app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
